f4f84b443d3d394fe6343dfa353affafc03bbc52
KoruzhynetsAnna <121615437+KoruzhynetsAnna@users.noreply.github.com>
2023-07-28 18:35:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,185 @@
+
+// var age = 13;
+
+// console.log(age);
+
+
+// var name = "Anya";
+
+// console.log(name);
+
+
+// var isStudent = true;
+
+// console.log(isStudent);
+
+
+// var myString = "Life is what happens when you're busy making other plans.";
+
+// console.log(myString);
+
+
+// var myNumber = 5;
+
+// myNumber += 10;
+
+// console.log(myNumber);
+
+// var myNull = null;
+
+// console.log(myNull);
+
+
+// var userName = prompt("Введіть ваше ім'я:");
+
+// alert("Привіт, " + userName + "!");
+
+
+// var confirmation = confirm("Ви погоджуєтесь з цією дією?");
+
+// if (confirmation) {
+//   alert("Дякую за підтвердження!");
+// } else {
+//   alert("Дію відмінено!");
+// }
+
+
+// alert("Ця дія небезпечна!");
+
+// var dangerConfirmation = confirm("Ви погоджуєтесь з цією дією?");
+
+// if (dangerConfirmation) {
+//   alert("Дякую за підтвердження!");
+// } else {
+//   alert("Дію відмінено!");
+// }
+
+
+
+// 1.
+
+// let tCelsius = 30;
+
+// let tFahrenheit = (tCelsius * 9/5) + 32;
+
+// console.log("Температура у Фаренгейтах:", tFahrenheit);
+
+
+// 2.
+
+// let daysInOneMonths = 30;
+
+// let hoursInOneMonths = daysInOneMonths * 24;
+
+// let minutesInOneMonths = hoursInOneMonths * 60;
+
+// console.log("К-ть годин у місяці:", hoursInOneMonths);
+
+// console.log("К-ть хвилин в місяці:", minutesInOneMonths);
+
+// 3.
+
+// let levelOfHealth = 100;
+
+// let levelOfEnergy = 100;
+
+// levelOfHealth -= 45;
+
+// levelOfEnergy -= 28;
+
+// console.log("рівень здоров'я:", levelOfHealth);
+
+// console.log("рівень енергії:", levelOfEnergy);
+
+// 4.
+
+// let purchaseAmount = 500;
+
+// let discountPercentage = 10;
+
+// let discountedAmount = purchaseAmount * (1 - discountPercentage/100);
+
+// console.log("сума зі знижкою:", discountedAmount);
+
+// 5.
+
+// let floatNumber = 4.90;
+
+// let roundNumber = Math.floor(floatNumber);
+
+// console.log("округлене число:", roundNumber);
+
+// 6.
+
+// let stringNumber = "8.67";
+
+// let decimalNumber = parseFloat(stringNumber);
+
+// console.log("десяткове число:", decimalNumber);
+
+// 7.
+
+// stringIdk = 15;
+
+// let idkNumber = parseInt(stringIdk);
+
+// console.log("Ціле число:", idkNumber);
+
+// let stringFromIdk = idkNumber.toString();
+
+// console.log("Рядок зі значенням:", stringFromIdk);
+
+// 8.
+
+// let number = 16;
+
+// let squareRoot = Math.sqrt(number);
+
+// console.log("Корінь числа:", squareRoot);
+
+// 9.
+
+// let integerValue = 45; 
+// let numberString = "123";
+
+// let parsedInteger = parseInt(numberString);
+// console.log("Ціле число після перетворення:", parsedInteger);
+
+// let stringFromInteger = integerValue.toString();
+// console.log("Рядок після перетворення:", stringFromInteger);
+
+1.
+
+let result = 5 + 5 + '5';
+
+console.log(typeof result);
+
+console.log(typeof result);
+
+2.
+
+let email = 'anetkor8@gmail.com';
+let hasAtSymbol = email.includes('@');
+let totalCharacters = email.length;
+
+console.log(`Емеіл мітить @ символ: ${hasAtSymbol}`);
+console.log(`Загальна к-ть символів в емейлі: ${totalCharacters}`);
+
+3.
+
+let word1 = 'My';
+let word2 = 'name';
+let word3 = 'is';
+
+let fullName = `${word1} ${word2} ${word3} Viktor`;
+console.log(fullName);
+
+4.
+
+let userName = 'Олександро';
+let payment = 300;
+
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
+
+
+
